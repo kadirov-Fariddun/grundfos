e77c18eb7fcd0de7af684625d4eff3e7eb3e9cda
Kadirov Fariddun <kadirovbrucelee@gmail.com>
2024-03-05 21:43:45 +0500

    db

---
+++ ProductsDB.js
@@ -0,0 +1,214 @@
+export const products = [
+    {
+        id:1,
+        title:'ALPHA',
+        info:`
+        Высокоэффективные циркуляционные насосы ALPHA  с регулируемой частотой вращения предназначены для систем отопления, кондиционирования воздуха и быт...
+        `,
+        type:'Циркуляционные насосы',
+        maxHead:'15 м',
+        maxCons:'4 м³/ч',
+        maxPress:'10 бар',
+        lTemperature:'0 .. 110 °C',
+    },
+    {
+        id:2,
+        title:'AP',
+        info:`
+        Насосы APG сконструированы для перекачивания сточных вод, воды с содержанием грязи, грунтовой и канализационной воды. Они снабжены системой измельч...
+        `,
+        type:'ПОГРУЖНЫЕ НАСОСЫ ДЛЯ СТОЧНЫХ ВОД',
+        maxHead:'68 м',
+        maxCons:'10 л/с',
+        maxPress:'6 бар',
+        lTemperature:'0 .. 40 °C',
+    },
+    {
+        id:3,
+        title:'BM',
+        info:`
+        Модуль повышения давления
+        `,
+        type:'НАСОСЫ С "МОКРЫМ" РОТОРОМ',
+        maxHead:'430 м',
+        maxCons:'165 м³/ч',
+        maxPress:'83 бар',
+        lTemperature:'0 .. 45 °C',
+    },
+    {
+        id:4,
+        title:'BMS',
+        info:`
+        Модуль повышения давления
+        `,
+        type:'ГОРИЗОНТАЛЬНЫЕ МНОГОСТУПЕНЧАТЫЕ НАСОСЫ',
+        maxHead:'872 м',
+        maxCons:'53 м³/ч',
+        lTemperature:'0 .. 40 °C',
+    },
+    {
+        id:5,
+        title:'BMSX',
+        info:`
+        Новая насосная система Grundfos BMSX имеет уникальный дизайн, предназначенный для опреснения морской воды и солоноватой воды.
+        `,
+    },
+    {
+        id:6,
+        title:'CIM/CIU',
+        info:`
+        Интерфейс передачи данных
+        `,
+    },
+    {
+        id:7,
+        title:'CM, CME',
+        info:`
+        CM, CME — это надежные, бесшумные и компактные горизонтальные насосы с односторонним всасыванием. Модульная конструкция насосов обеспечивает незатр...
+        `,
+        type:'КОНСОЛЬНО-МОНОБЛОЧНЫЙ МНОГОСТУПЕНЧАТЫЙ',
+        maxHead:'136 м',
+        maxCons:'36 м³/ч',
+        maxPress:'16 бар',
+        lTemperature:'-20 .. 120 °C',
+    },
+    {
+        id:8,
+        title:'CMB, CMBE Насосные установки',
+        info:`
+        Компактные насосные подкачивающие установки CMB, CMBE предназначены для систем бытового и непромышленного коммерческого водоснабжения. Модульная ко...
+        `,
+        type:'УСТАНОВКА ПОВЫШЕНИЯ ДАВЛЕНИЯ',
+        maxHead:'102 м',
+        maxCons:'17 м³/ч',
+        maxPress:'10 бар',
+        lTemperature:'0 .. 60 °C',
+    },
+    {
+        id:9,
+        title:'COMFORT',
+        info:`
+        Энергоэффективные рециркуляционные насосы COMFORT предназначены для подачи горячей воды в одно- и двухквартирные дома. Данные насосы подходят для п...
+        `,
+        type:'ЦИРКУЛЯЦИОННЫЕ НАСОСЫ',
+        maxHead:'1 м',
+        maxCons:'0.50 м³/ч',
+        maxPress:'10 бар',
+        lTemperature:'0 .. 95 °C',
+    },
+    {
+        id:10,
+        title:'Conlift',
+        info:`
+        Насосы для откачки конденсата CONLIFT1 предназначены для перекачивания конденсата, образующегося ниже уровня канализационного коллектора или не пос...
+        `,
+        maxHead:'6 м',
+        maxCons:'0.17 л/с',
+        lTemperature:'0 .. 50 °C',
+    },
+    {
+        id:11,
+        title:'Control CUE, DC, DDD, MP 204, MPC',
+        info:`
+        Комплексные решения по контролю для различных областей применения
+        `,
+    },
+    {
+        id:12,
+        title:'Control DC',
+        info:`
+        Complete Control solutions, with cabinet, controller and switchgear for various application.
+        `,
+    },
+    {
+        id:13,
+        title:'Control MP 204',
+        info:`
+        Система управления насосами, предназначенна для систем водоснабжения и водоотведения. Обеспечивается полная защита одного насоса.
+        `,
+    },
+    {
+        id:14,
+        title:'CR, CRE, CRI, CRIE, CRN, CRNE, CRT, CRTE',
+        info:`
+        Вертикальные многоступенчатые центробежные насосы с всасывающим и напорным патрубками на одном уровне.
+        `,
+        type:'МНОГОСТУПЕНЧАТЫЕ НАСОСЫ СЕРИИ "ИН-ЛАЙН"',
+        maxHead:'410 м',
+        maxCons:'336 м³/ч',
+        maxPress:'50 бар',
+        lTemperature:'-40 .. 150 °C',
+    },
+    {
+        id:15,
+        title:'CU 100',
+        info:`
+        Принадлежности
+        `,
+    },
+    {
+        id:16,
+        title:'CU 241, 242, 362',
+        info:`
+        Control units designed for level control, monitoring and protection of waste water and bore hole applications. For integration in to cabinets.
+        `,
+    },
+    {
+        id:17,
+        title:'CU 300, 301',
+        info:`
+        Designed for monitoring and control of SQE submersible pumps e.g. for constant pressure applications.
+        `,
+    },
+    {
+        id:18,
+        title:'CUE',
+        info:`
+        CUE — это линейка преобразователей частоты, предназначенных для управления насосами Grundfos.
+        `,
+    },
+    {
+        id:19,
+        title:'DP',
+        info:`
+        Дренажные насосы
+        `,
+        type:'ПОГРУЖНЫЕ НАСОСЫ ДЛЯ СТОЧНЫХ ВОД',
+        maxHead:'25 м',
+        maxCons:'14 л/с',
+        maxPress:'6 бар',
+        lTemperature:'0 .. 40 °C',
+    },
+    {
+        id:20,
+        title:'DPI',
+        info:`
+        Датчики перепада давления серии DPI отличаются точностью и надежностью. Работают в соответствии с принципом пьезорезистивного измерения.
+        `,
+        lTemperature:'-30 .. 120 °C',
+    },
+    {
+        id:21,
+        title:'DPK',
+        info:`
+        Дренажные насосы
+        `,
+        type:'ПОГРУЖНЫЕ НАСОСЫ ДЛЯ СТОЧНЫХ ВОД',
+        maxHead:'59 м',
+        maxCons:'46 л/с',
+        maxPress:'6 бар',
+        lTemperature:'0 .. 40 °C',
+    },
+    {
+        id:22,
+        title:'DPS',
+        info:`
+        DPS is a differential pressure sensor designed to be robust, affordable, accurate and compact. It uses our Silicoat® coating to be in direct contac...
+        `,
+        lTemperature:'-10 .. 120 °C',
+    },
+
+
+
+]
+
